3f22798ec3ca152590892f382e85480c905be8eb
Olafcio <75330538+Olafcio1@users.noreply.github.com>
2026-06-26 22:09:49 +0200

    Fix install.py

--- install.py
+++ install.py
@@ -1,7 +1,17 @@
 import subprocess
+import os
 
 profile = subprocess.run(["powershell", "-NoProfile", "-NoLogo", "-Command", "echo $profile"], stdout=subprocess.PIPE, text=True).stdout.rstrip()
 path = "/".join(__file__.replace("\\", "/").split("/")[:-1])
 
-with open(profile, "a", encoding="utf-8") as f:
-  f.write("\nfunction funcutter() { python \"%s\" $args }" % path)
+mode = "a"
+prefix = "\n"
+
+if not os.path.isfile(profile):
+  mode = "w"
+  prefix = ""
+
+  os.makedirs(os.path.dirname(profile), exist_ok=True)
+
+with open(profile, mode, encoding="utf-8") as f:
+  f.write(prefix + "function funcutter() { python \"%s\" $args }" % path)
